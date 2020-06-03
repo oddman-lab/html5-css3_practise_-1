70613e08cccbaabc2d33e6ff57c9681f32eae276
Ilya <ilya.shcoder@gmail.com>
2020-06-03 22:40:27 +0300

    страшные эксперименты с управлением СКВ

--- admin-panel/js/script.js
+++
@@ -1,7 +0,0 @@
-let title = document.querySelector('.side-in');
-let sideBar = document.querySelector('.sidebar-menu');
-
-title.addEventListener('click', () => {
-  title.classList.toggle('side-in-hover');
-  sideBar.classList.toggle('animation_hide');
-});
